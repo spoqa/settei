a06efa6ac80c31c5d3e9a068de050f8f1404d77d
Hong Minhee <hongminhee@member.fsf.org>
2016-04-15 22:18:01 +0900

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name='settei',
-    version='0.1.0',
+    version='0.1.1',
     description='Configuration loader from a TOML file',
     license='Apache 2.0',
     author='Spoqa Creators',
